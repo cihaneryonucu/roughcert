b3bc4e26a76a30996e87ec84ff59fe2bf32fa569
Marco Spanghero <marcosp@kth.se>
2021-03-02 21:34:06 +0100

    Initial version of the contact-keeping server
    Basic logic for the contact keeping server

--- server.py
+++ server.py
@@ -1,88 +1,42 @@
-if __name__ == '__main__':
-    raise Exception("This module cannot be run as an executable!")
-
 import zmq
-import threading
+import sys
 
-import message_pb2
+import contacts_pb2 as cpb
 
-
-
-class Sender(object):
-    def __init__(self, chat_address, chat_port, chat_pipe):
-        self.chat_address = chat_address
-        self.chat_port = chat_port
-        self.context = zmq.Context()
-        self.tx_sock = None
-        self.chat_pipe = chat_pipe
-
-    def connect(self):
-        self.tx_sock = zmq.context.socket(zmq.PUB)
-        connect_string = 'tcp://{}:{}'.format(
-            self.chat_address, self.chat_port)
-        self.tx_sock.connect(connect_string)
-
-    def reconnect_to_server(self):
-        self.tx_sock.setsockopt(zmq.LINGER, 0)
-        self.tx_sock.close()
-        self.connect()
-
-    def get_new_message(self):
-        return self.chat_pipe.recv_string()
-
-    def send_message(self, message):
-        self.tx_sock.send(message)
-
-    def receive_message(self):
-        self.tx_sock.recv()
-
-    def sender_loop(self):
-        self.connect()
-        while True:
-            message = self.get_new_message()
-            self.send_message(message)
-
-    def run(self):
-        thread = threading.Thread(target=self.sender_loop())
-        thread.daemon = True
-        thread.start()
-
-
-class Receiver(object):
-    def __init__(self, chat_port, chat_pipe):
-        self.chat_port = chat_port
-        self.context = zmq.Context()
-        self.rx_sock = None
-        self.history = chat_pipe
+class Server(object):
+    def __init__(self, port):
+        self.port = port
+        self.userList = []
+        self.socket = None
         self.poller = zmq.Poller()
 
     def connect(self):
-        self.rx_sock = zmq.context.socket(zmq.SUB)
-        connect_string = 'tcp://*:{}'.format(self.chat_port)
-        self.rx_sock.connect(connect_string)
+        context = zmq.Context()
+        self.socket = context.socket(zmq.REP)
+        connect_string = 'tcp://*:{}'.format(self.port)
+        self.socket.bind(connect_string)
+        self.poller.register(self.socket, zmq.Poller.POLLIN)
 
-    def push_message_to_history(self, message):
-        self.history.send(message)
-
-    def register_with_poller(self):
-        self.poller.register(self.rx_sock, zmq.POLLIN)
-
-    def has_message(self):
-        events = dict(self.poller.poll(3000))
-        return events.get(self.rx_sock) == zmq.POLLIN
-
-    def get_message(self):
-        return self.rx_sock.recv()
-
-    def receiver_loop(self):
-        self.connect()
-        self.register_with_poller()
+    def receive_command(self):
+        action = cpb.server_action()
         while True:
-            if self.has_message():
-                message = self.get_message()
-                self.push_message_to_history(message)
+            socks = dict(self.poller.poll())
+            if self.socket in socks and socks[self.socket] == zmq.POLLIN:
+                action.ParseFromString(self.socket.recv())
+                self.parse_command(action)
 
-    def run(self):
-        thread = threading.Thread(target=self.receiver_loop)
-        thread.daemon = True
-        thread.start()
+    def parse_command(self, action):
+        if action.action == 'CTS':
+            pass #request all contacts on the server currently
+        if action.action == 'REG':
+            #Register a new contact on the server - check if already present
+            if action.contact.user not in self.userList:
+                self.userList.append(action.contact.user)
+            else:
+                pass
+        if action.action == 'DEL':
+            #Remove the contact from the server - throw exception if contact not present
+            try:
+                self.userList.remove(action.contact.user)
+            except ValueError:
+                print("User not in list")
