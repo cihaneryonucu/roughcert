9533940ae4277172d65361b9d443f52203e76cf8
Marco Spanghero <marcosp@kth.se>
2021-03-02 22:01:57 +0100

    Fixed server so that it always ACK's to all requests

--- server.py
+++ server.py
@@ -17,7 +17,7 @@ class Server(object):
         self.socket.bind(connect_string)
         self.poller.register(self.socket, zmq.Poller.POLLIN)
 
-    def receive_command(self):
+    def server_loop(self):
         action = cpb.server_action()
         while True:
             socks = dict(self.poller.poll())
@@ -25,18 +25,28 @@ class Server(object):
                 action.ParseFromString(self.socket.recv())
                 self.parse_command(action)
 
-    def parse_command(self, action):
-        if action.action == 'CTS':
-            pass #request all contacts on the server currently
-        if action.action == 'REG':
-            #Register a new contact on the server - check if already present
+    def parse_command(self, action, socket):
+        reply = cpb.server_action()
+        if action.action == 'CTS':                   #request all contacts on the server currently
+            reply.action = 'ACK'
+            for users in self.userList:
+                reply.contact.user.add().user = users
+                socket.send(reply)
+        elif action.action == 'REG':                 #Register a new contact on the server - check if already present
             if action.contact.user not in self.userList:
                 self.userList.append(action.contact.user)
             else:
                 pass
-        if action.action == 'DEL':
-            #Remove the contact from the server - throw exception if contact not present
+            reply.action = 'ACK'
+            socket.send(reply)
+        elif action.action == 'DEL':                  #Remove the contact from the server - throw exception if contact not present
             try:
                 self.userList.remove(action.contact.user)
             except ValueError:
                 print("User not in list")
+            reply.action = 'ACK'
+            socket.send(reply)
+        elif action.action == 'ACK':
+            print('Client ACk\'d our reply')
+        else:
+            print("Request malformed - nothing to do")
