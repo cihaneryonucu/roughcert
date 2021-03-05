9149dca039adc969258e594abe61d25cebc59b0f
Marco Spanghero <marcosp@kth.se>
2021-03-05 11:14:57 +0100

    refactor chat Sender/Receiver classes

--- chat.py
+++ chat.py
@@ -9,17 +9,16 @@ from queue import SimpleQueue
 
 
 class Sender(object):
-    def __init__(self, chat_address, chat_port, outbox):
-        self.chat_address = chat_address
-        self.chat_port = chat_port
-        self.context = zmq.Context()
+    def __init__(self, remote_peer_address, remote_peer_port, outbox):
+        self.remote_peer_address = remote_peer_address
+        self.remote_peer_port = remote_peer_port
         self.tx_sock = None
-        self.chat_pipe = outbox
-        print(self.chat_address, self.chat_port)
+        self.outbox = outbox
+        print(self.remote_peer_address, self.remote_peer_port)
 
     def connect(self):
         self.tx_sock = zmq.Context().instance().socket(zmq.PAIR)
-        self.tx_sock.connect('tcp://{}:{}'.format(self.chat_address, self.chat_port))
+        self.tx_sock.connect('tcp://{}:{}'.format(self.remote_peer_address, self.remote_peer_port))
         
 
     def receive_message(self):
@@ -27,7 +26,7 @@ class Sender(object):
 
     def sender_loop(self):
         while True:
-            message = self.chat_pipe.get()
+            message = self.outbox.get()
             self.tx_sock.send_string(message, flags=zmq.NOBLOCK)
 
     def outboxQueue(self):
@@ -41,23 +40,21 @@ class Sender(object):
 
 
 class Receiver(object):
-    def __init__(self,chat_address, chat_port, inbox):
-        self.chat_address = chat_address
-        self.chat_port = chat_port
-        self.context = zmq.Context()
+    def __init__(self, local_chat_address, local_chat_port, inbox):
+        self.local_chat_address = local_chat_address
+        self.local_chat_port = local_chat_port
         self.rx_sock = None
-        self.history = inbox
-        self.poller = zmq.Poller()
+        self.inbox = inbox
 
     def connect(self):
         self.rx_sock = zmq.Context().instance().socket(zmq.PAIR)
-        self.rx_sock.bind('tcp://{}:{}'.format(self.chat_address, self.chat_port))
+        self.rx_sock.bind('tcp://{}:{}'.format(self.local_chat_address, self.local_chat_port))
 
 
     def receiver_loop(self):
         while True:
             message = self.rx_sock.recv_string()
-            self.history.put(message)
+            self.inbox.put(message)
 
     def inboxQueue(self):
         return self.inboxQueue
