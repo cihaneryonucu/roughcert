b211a73119779cbeffa7e76b052c372d9a2061b4
cihaneryonucu <eryonucu@gmail.com>
2021-03-06 23:26:14 +0100

    TLS like key derivation: Round 2

--- Crypto.py
+++ Crypto.py
@@ -12,7 +12,7 @@ from cryptography.x509 import NameOID
 from cryptography.hazmat.primitives import hashes
 
 
-def initiate_key_derivation(target_addr, target_port):
+def initiate_key_derivation(target_addr, target_port, client_cert):
     # Connect
     tx_sock = zmq.Context().instance().socket(zmq.PAIR)
     tx_sock.connect('tcp://{}:{}'.format(target_addr, target_port))
@@ -21,8 +21,14 @@ def initiate_key_derivation(target_addr, target_port):
     secret = secrets.token_urlsafe(16)
     tx_sock.send_string('hey' + secret, flags=zmq.NOBLOCK)
 
+    # Round 2 listen
+    message = tx_sock.recv()
+    print(message)
+    message = tx_sock.recv()
+    print(message)
 
-def listen_key_derivation(addr, port):
+
+def listen_key_derivation(addr, port, server_cert):
     # Connect
     rx_sock = zmq.Context().instance().socket(zmq.PAIR)
     rx_sock.bind('tcp://{}:{}'.format(addr, port))
@@ -32,12 +38,9 @@ def listen_key_derivation(addr, port):
     print(message)
 
     #Round 2 send cert, response and secret
-
-
-if sys.argv[1] == 's':
-    listen_key_derivation(sys.argv[2], sys.argv[3])
-else:
-    initiate_key_derivation(sys.argv[2], sys.argv[3])
+    secret = secrets.token_urlsafe(16)
+    rx_sock.send_string('hey back' + secret, flags=zmq.NOBLOCK)
+    rx_sock.send(server_cert.public_bytes(serialization.Encoding.PEM), flags=zmq.NOBLOCK)
 
 
 def generate_private_key(filename):
@@ -155,12 +158,16 @@ def import_private_key(filename):
                                                      default_backend())  # todo: replace password with getpass().encode("utf-8")
     return private_key
 
-
-# key = generate_private_key('priv')
+key = generate_private_key('priv')
 details = {'country': 'Se', 'region': 'Skane', 'city': 'stockholm', 'org': 'someCo', 'hostname': 'somesite.com'}
-# # cert = generate_self_signed_cert(key,'pub',details,10)
+cert = generate_self_signed_cert(key, 'pub', details, 10)
 # key = import_private_key('priv')
 # cert = import_certificate('pub')
 # csr = create_csr(key,details)
 
 # print(sign_csr(csr,cert,key,10))
+
+if sys.argv[1] == 's':
+    listen_key_derivation(sys.argv[2], sys.argv[3], cert)
+else:
+    initiate_key_derivation(sys.argv[2], sys.argv[3], '')
