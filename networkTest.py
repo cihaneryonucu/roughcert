e8f8c50611188819d746a8f6838ca85528e24b6b
Marco Spanghero <marcosp@kth.se>
2021-03-08 12:30:36 +0100

    changed test class init method

--- networkTest.py
+++ networkTest.py
@@ -4,16 +4,16 @@ from connectionManager import connection_manager
 import server
 
 class TestNetworking(unittest.TestCase):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, *args, **kwargs):
+        super(TestNetworking, self).__init__(*args, **kwargs)
         self.contactList = [{"username" : "marco", "ipAddr" : "10.0.0.1", "port" : "10000"}]
         self.local_user = {"username" : "marco", "ipAddr" : "10.0.0.1", "port" : "10000"}
         self. connection_manager = None
-        self.server = server.Server()
+        self.server = server.Server(port=9998)
         self.server.run()
 
     def setup_testing_system(self):
-        self.connection_manager = connection_manager("127.0.0.1", local_user=self.local_user, port=10040)
+        self.connection_manager = connection_manager("127.0.0.1", local_user=self.local_user, port=9998)
 
     def test_register_user(self):
         self.assertEqual(self.connection_manager.register_user(), "ACK", "Server ack'd correctly")
@@ -24,6 +24,8 @@ class TestNetworking(unittest.TestCase):
 
     def test_remove_user(self):
         self.assertEqual(self.connection_manager.remove_user(), "ACK", "Server ack'd correctly")
+        self.assertEqual(self.connection_manager.contactList, [], "Contact Lists are equal - only one person should be avaialble")
+
 
 if __name__ == '__main__':
     unittest.main()
