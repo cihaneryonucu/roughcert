9121942c876ae1270095b97c554d75651539c3db
Marco Spanghero <marcosp@kth.se>
2021-03-08 11:09:47 +0100

    Fixed test fixture for connection_manage testing

--- networkTest.py
+++ networkTest.py
@@ -2,21 +2,20 @@ import unittest
 
 from connectionManager import connection_manager
 
-class TestNetworking(unittest.TestCase){
+
+class TestNetworking(unittest.TestCase):
+    def __init__(self):
+        super().__init__()
+        self.contactList = [{"username" : "marco", "ipAddr" : "10.0.0.1", "port" : "10000"}]
+        self.local_user = {"username" : "marco", "ipAddr" : "10.0.0.1", "port" : "10000"}
+        self. connection_manager = None
 
     def setup_testing_system(self):
-        connection_manager("127.0.0.1", local_user=local_user)
+        self.connection_manager = connection_manager("127.0.0.1", local_user=self.local_user)
 
     def test_register_user(self):
+        self.assertEqual(self.connection_manager.register_user(), "ACK", "Server ack'd correctly")
 
-        contactList = [{"username" : "marco", "ipAddr" : "10.0.0.1", "port" : "10000"}]
-        local_user = {"username" : "marco", "ipAddr" : "10.0.0.1", "port" : "10000"}
-        connection_manager.register_user(local_user)
-
-        pass
-
-
-}
-
+        
 if __name__ == '__main__':
     unittest.main()
