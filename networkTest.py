0e71cdd514a1245f68cbc602707769c42cb5e379
Marco Spanghero <marcosp@kth.se>
2021-03-08 10:59:24 +0100

    started testcases definition

--- networkTest.py
+++ networkTest.py
@@ -4,18 +4,14 @@ from connectionManager import connection_manager
 
 class TestNetworking(unittest.TestCase){
 
+    def setup_testing_system(self):
+        connection_manager("127.0.0.1", local_user=local_user)
+
     def test_register_user(self):
 
         contactList = [{"username" : "marco", "ipAddr" : "10.0.0.1", "port" : "10000"}]
         local_user = {"username" : "marco", "ipAddr" : "10.0.0.1", "port" : "10000"}
-        #setup socket for tessting
-
-        
-
-
-        connection_manager("127.0.0.1", local_user=local_user)
-
-
+        connection_manager.register_user(local_user)
 
         pass
 
