920af0dc9b32adc094129533a9346e8d3cbee105
Cihan <eryonucu@gmail.com>
2021-03-10 16:23:03 +0100

    Test: fixing the import private key test and description
    Added test description and generating the key before importing

--- CryptoTesting.py
+++ CryptoTesting.py
@@ -7,8 +7,12 @@ class CryptoTesting(unittest.TestCase):
     def test_is_private_key(self):
         self.assertIsInstance(Crypto.generate_private_key('testKey.pem'),Crypto.rsa.RSAPrivateKey)
 
+    # Test the import function whether it is a RSA private key, meaning it importaed correctly or not
     def test_is_imported_key_private_key(self):
+        Crypto.generate_private_key('testKey.pem')
         self.assertIsInstance(Crypto.import_private_key('testKey.pem'),Crypto.rsa.RSAPrivateKey)
+
+    
 
 if __name__ == '__main__':
     unittest.main()
